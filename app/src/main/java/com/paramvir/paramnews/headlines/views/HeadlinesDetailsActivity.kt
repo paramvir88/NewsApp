68d0d418854dd52bac04291baf0d17dc90a4f640
paramvir <psingh@alhilalbank.ae>
2024-07-02 13:35:22 +1000

    saving article in file storage ( initial)

--- app/src/main/java/com/paramvir/paramnews/headlines/views/HeadlinesDetailsActivity.kt
+++ app/src/main/java/com/paramvir/paramnews/headlines/views/HeadlinesDetailsActivity.kt
@@ -1,11 +1,14 @@
 package com.paramvir.paramnews.headlines.views
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.paramvir.paramnews.databinding.ActivityHeadlinesDetailsBinding
 import com.paramvir.paramnews.headlines.HEADLINE_URL
 
-
+/**
+ * To show the clicked news article in Embedded WebView.
+ */
 class HeadlinesDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHeadlinesDetailsBinding
@@ -17,5 +20,18 @@ class HeadlinesDetailsActivity : AppCompatActivity() {
         url?.let {
             binding.showHeadlinesDetails.loadUrl(url)
         }
+
+        binding.saveButton.setOnClickListener { // Implement your save logic here
+            url?.let { saveArticle(it) }
+        }
     }
+
+    private fun saveArticle(url: String) {
+
+        FileStorageHelper.saveUrlToFile(this, url)
+        Toast.makeText(this, "This article is saved.", Toast.LENGTH_SHORT).show()
+    }
+
+
+
 }
